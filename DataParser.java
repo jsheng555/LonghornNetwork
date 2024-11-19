dc787b1b197081a6f3a446895178ef7ba4d15e36
jsheng555 <41525117+jsheng555@users.noreply.github.com>
2024-11-19 12:25:44 -0600

    Delete DataParser.java

--- DataParser.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-import java.util.*;
-
-public class DataParser {
-    public static List<UniversityStudent> parseStudents(String filename) throws IOException {
-        return new ArrayList<>();
-    }
-}
